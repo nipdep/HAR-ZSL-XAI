cecb253aacff4c61d2c3c1faf8fa8fe62221c006
nipdep <nipun1deelaka@gmail.com>
2023-01-14 10:04:23 +0530

    [add] mhealth reader

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -86,3 +86,59 @@ class SupConLoss(nn.Module):
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
+
+
+class SoftNearestNeighbours(torch.nn.Module):
+    def __init__(self, x, y, temperature_init: float = 0.1, eps: float = 1e-8, raise_on_inf=False, raise_on_single_point_for_class=False, cosine=False):
+        super(SoftNearestNeighbours, self).__init__()
+        weights = torch.zeros(1) + 1/temperature_init
+        self.weights = torch.nn.Parameter(weights)
+        self.x = x
+        self.y = y
+        self.n = self.x.shape[0]
+        if cosine:
+            self.distances = self.pairwise_cosine_similarity(x)
+        else:
+            self.distances = torch.cdist(x, x, p=2)
+        self.eps = eps
+        self.raise_on_inf = raise_on_inf
+        self.raise_on_single_point_for_class = raise_on_single_point_for_class
+
+    @staticmethod
+    def pairwise_cosine_similarity(x):
+        dots = x @ x.T
+        inv_magnitude = torch.sqrt(1 / dots.diag())
+        cosines = (dots * inv_magnitude).T * inv_magnitude
+        return cosines
+
+    def get_bool_mask(self, idx, class_subset=False):
+        if class_subset:
+            mask = self.y == self.y[idx]
+        else:
+            mask = torch.ones(self.n, dtype=bool)
+        mask[idx] = False
+        return mask
+
+    def forward(self):
+        fracs = torch.zeros(self.n)
+        exponents = torch.exp(-self.distances * self.weights[0])
+        ignore_idx = []
+        for i in range(self.n):
+            top = exponents[i, self.get_bool_mask(i, True)]
+            bot = exponents[i, self.get_bool_mask(i, False)]
+            fracs[i] = torch.log(top.sum()) - torch.log(bot.sum()+self.eps)
+            if len(top) == 0:
+                if self.raise_on_single_point_for_class:
+                    raise ValueError("No other points with the same label in batch")
+                else:
+                    ignore_idx.append(i)
+            elif fracs[i].isnan().item():
+                raise ValueError("Nan detected in loss calculation")
+            elif fracs[i].isinf().item() and self.raise_on_inf:
+                raise ValueError("inf detected in loss calculation, if optimising try reducing the lr")
+        ignore_mask = torch.ones(self.n, dtype=torch.bool)
+        for i in ignore_idx:
+            ignore_mask[i] = False
+        fracs = fracs[ignore_mask]
+        loss = (-1 / len(fracs)) * fracs.sum()
+        return loss
