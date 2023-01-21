273783d4d9d86263606f8f94876726d8af58de57
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2023-01-21 07:48:02 +0530

    My edits

---
+++ src/datasets/data.py
@@ -0,0 +1,227 @@
+import os
+import json
+import random
+import math
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from copy import deepcopy
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn, Tensor
+from torch.nn import functional as F
+from torch.optim import Adam
+
+from scipy.signal import resample
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+
+import matplotlib.pyplot as plt
+
+
+# build PAMAP2 dataset data reader
+class PAMAP2Reader(object):
+    def __init__(self, root_path):
+        self.root_path = root_path
+        self.readPamap2()
+
+    def readFile(self, file_path):
+        all_data = {"data": {}, "target": {}, 'collection': []}
+        prev_action = -1
+        starting = True
+        # action_seq = []
+        action_ID = 0
+
+        for l in open(file_path).readlines():
+            s = l.strip().split()
+            if s[1] != "0":
+                if (prev_action != int(s[1])):
+                    if not (starting):
+                        df = pd.DataFrame(action_seq)
+                        intep_df = df.interpolate(method='linear', limit_direction='backward', axis=0)
+                        intep_data = intep_df.values
+                        all_data['data'][action_ID] = np.array(intep_data)
+                        all_data['target'][action_ID] = prev_action
+                        action_ID += 1
+                    action_seq = []
+                else:
+                    starting = False
+                data_seq = np.array(s[3:]).astype(np.float16)
+                # data_seq[np.isnan(data_seq)] = 0
+                action_seq.append(data_seq)
+                prev_action = int(s[1])
+                # print(prev_action)
+                all_data['collection'].append(data_seq)
+        else:
+            if len(action_seq) > 1:
+                df = pd.DataFrame(action_seq)
+                intep_df = df.interpolate(method='linear', limit_direction='backward', axis=0)
+                intep_data = intep_df.values
+                all_data['data'][action_ID] = np.array(intep_data)
+                all_data['target'][action_ID] = prev_action
+        return all_data
+
+    def readPamap2Files(self, filelist, cols, labelToId):
+        data = []
+        labels = []
+        collection = []
+        for i, filename in enumerate(filelist):
+            print('Reading file %d of %d' % (i + 1, len(filelist)))
+            fpath = os.path.join(self.root_path, filename)
+            file_data = self.readFile(fpath)
+            data.extend(list(file_data['data'].values()))
+            labels.extend(list(file_data['target'].values()))
+            collection.extend(file_data['collection'])
+        return np.asarray(data), np.asarray(labels, dtype=int), np.array(collection)
+
+    def readPamap2(self):
+        files = ['subject101.dat', 'subject102.dat', 'subject103.dat', 'subject104.dat', 'subject105.dat',
+                 'subject106.dat', 'subject107.dat', 'subject108.dat', 'subject109.dat', 'subject110.dat',
+                 'subject111.dat', 'subject112.dat', 'subject113.dat', 'subject114.dat']
+
+        label_map = [
+            # (0, 'other'),
+            (1, 'lying'),
+            (2, 'sitting'),
+            (3, 'standing'),
+            (4, 'walking'),
+            (5, 'running'),
+            (6, 'cycling'),
+            (7, 'Nordic walking'),
+            (9, 'watching TV'),
+            (10, 'computer work'),
+            (11, 'car driving'),
+            (12, 'ascending stairs'),
+            (13, 'descending stairs'),
+            (16, 'vacuum cleaning'),
+            (17, 'ironing'),
+            (18, 'folding laundry'),
+            (19, 'house cleaning'),
+            (20, 'playing soccer'),
+            (24, 'rope jumping')
+        ]
+        labelToId = {x[0]: i for i, x in enumerate(label_map)}
+        # print "label2id=",labelToId
+        idToLabel = [x[1] for x in label_map]
+        # print "id2label=",idToLabel
+        cols = [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34,
+            35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53
+        ]
+        # print "cols",cols
+        self.data, self.targets, self.all_data = self.readPamap2Files(files, cols, labelToId)
+        # print(self.data)
+        # nan_perc = np.isnan(self.data).astype(int).mean()
+        # print("null value percentage ", nan_perc)
+        # f = lambda x: labelToId[x]
+        print(np.unique(self.targets))
+        self.targets = np.array([labelToId[i] for i in list(self.targets)])
+        print(np.unique(self.targets))
+        self.label_map = label_map
+        self.idToLabel = idToLabel
+        # return data, idToLabel
+
+    def aggregate(self, signal):
+        means = signal.mean(axis=0)
+        # print(means.shape)
+        stds = signal.std(axis=0)
+        # print(stds.shape)
+        mergered = np.vstack((means, stds)).reshape((-1,), order='F')
+        return mergered
+
+    def windowing(self, signal, window_len, overlap):
+        seq_len = int(window_len * 100)  # 100Hz compensation
+        overlap_len = int(overlap * 100)  # 100Hz
+        l, _ = signal.shape
+        if l > seq_len:
+            windowing_points = np.arange(start=0, stop=l - seq_len, step=seq_len - overlap_len, dtype=int)[:-1]
+            # windowing_points = windowing_points-overlap_len
+            # windowing_points[0] = 0
+
+            windows = [signal[p:p + seq_len, :] for p in windowing_points]
+        else:
+            windows = []
+        return windows
+
+    def resampling(self, data, targets, window_size, window_overlap, resample_freq):
+        assert len(data) == len(targets), "# action data & # action labels are not matching"
+        all_data, all_ids, all_labels = [], [], []
+        for i, d in enumerate(data):
+            # print(">>>>>>>>>>>>>>>  ", np.isnan(d).mean())
+            label = targets[i]
+            windows = self.windowing(d, window_size, window_overlap)
+            for w in windows:
+                # print(np.isnan(w).mean(), label, i)
+                resample_sig = self.aggregate(w)
+                # print(np.isnan(resample_sig).mean(), label, i)
+                all_data.append(resample_sig)
+                all_ids.append(i + 1)
+                all_labels.append(label)
+
+        return all_data, all_ids, all_labels
+
+    def generate(self, unseen_classes, resampling=True, window_size=5.21, window_overlap=1, resample_freq=10,
+                 seen_ratio=0.2, unseen_ratio=0.8):
+        # assert all([i in list(self.label_map.keys()) for i in unseen_classes]), "Unknown Class label!"
+        seen_classes = [i for i in range(len(self.idToLabel)) if i not in unseen_classes]
+        unseen_mask = np.in1d(self.targets, unseen_classes)
+
+        s = np.unique(self.targets, return_counts=True)
+        print("per class count : ", dict(zip([self.idToLabel[i] for i in s[0]], s[1])))
+
+        # build seen dataset
+        seen_data = self.data[np.invert(unseen_mask)]
+        seen_targets = self.targets[np.invert(unseen_mask)]
+
+        # build unseen dataset
+        unseen_data = self.data[unseen_mask]
+        unseen_targets = self.targets[unseen_mask]
+
+        # resampling seen and unseen datasets
+        seen_data, seen_ids, seen_targets = self.resampling(seen_data, seen_targets, window_size, window_overlap,
+                                                            resample_freq)
+        unseen_data, unseen_ids, unseen_targets = self.resampling(unseen_data, unseen_targets, window_size,
+                                                                  window_overlap, resample_freq)
+
+        seen_data, seen_targets = np.array(seen_data), np.array(seen_targets)
+        unseen_data, unseen_targets = np.array(unseen_data), np.array(unseen_targets)
+        # train-val split
+        seen_index = list(range(len(seen_targets)))
+        random.shuffle(seen_index)
+        split_point = int((1 - seen_ratio) * len(seen_index))
+        fst_index, sec_index = seen_index[:split_point], seen_index[split_point:]
+        print(type(fst_index), type(sec_index), type(seen_data), type(seen_targets))
+        X_seen_train, X_seen_val, y_seen_train, y_seen_val = seen_data[fst_index, :], seen_data[sec_index, :], \
+            seen_targets[fst_index], seen_targets[sec_index]
+
+        # val-test split
+        unseen_index = list(range(len(unseen_targets)))
+        random.shuffle(unseen_index)
+        split_point = int((1 - unseen_ratio) * len(unseen_index))
+        fst_index, sec_index = unseen_index[:split_point], unseen_index[split_point:]
+
+        X_unseen_val, X_unseen_test, y_unseen_val, y_unseen_test = unseen_data[fst_index, :], unseen_data[sec_index, :], \
+            unseen_targets[fst_index], unseen_targets[sec_index]
+
+        data = {'train': {
+            'X': X_seen_train,
+            'y': y_seen_train
+        },
+            'eval-seen': {
+                'X': X_seen_val,
+                'y': y_seen_val
+            },
+            'test': {
+                'X': unseen_data,
+                'y': unseen_targets
+            },
+            'seen_classes': seen_classes,
+            'unseen_classes': unseen_classes
+        }
+
+        return data
---
+++ src/datasets/dataset.py
@@ -0,0 +1,35 @@
+from numpy import random
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class PAMAP2Dataset(Dataset):
+    def __init__(self, data, actions, attributes, attribute_dict, action_classes, seq_len=120):
+        super(PAMAP2Dataset, self).__init__()
+        self.data = torch.from_numpy(data)
+        self.actions = actions
+        self.attribute_dict = attribute_dict
+        self.seq_len = seq_len
+        self.attributes = torch.from_numpy(attributes)
+        self.action_classes = action_classes
+        # build action to id mapping dict
+        self.n_action = len(self.actions)
+        self.action2Id = dict(zip(action_classes, range(self.n_action)))
+
+    def __getitem__(self, ind):
+        x = self.data[ind, ...]
+        target = self.actions[ind]
+        y = torch.from_numpy(np.array([self.action2Id[target]]))
+        # extraction semantic space generation skeleton sequences
+        vid_idx = random.choice(self.attribute_dict[target])
+        y_feat = self.attributes[vid_idx, ...]
+        return x, y, y_feat
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def getClassAttrs(self):
+        sampling_idx = [random.choice(self.attribute_dict[i]) for i in self.action_classes]
+        ft_mat = self.attributes[sampling_idx, ...]
+        return ft_mat
